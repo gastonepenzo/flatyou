ecf8081901d77e8c419b1ef87555e9a0b0347bae
Gastone Penzo <gastone.penzo@gmail.com>
2017-11-04 04:06:30 +0100

    Aggiunte configurazioni servizi e routing di test

---
+++ application/index.php
@@ -0,0 +1,31 @@
+<?php
+
+function send($url,$params)
+{
+   $postData = '';
+   //create name value pairs seperated by &
+   foreach($params as $k => $v) 
+   { 
+      $postData .= $k . '='.$v.'&'; 
+   }
+   $postData = rtrim($postData, '&');
+ 
+   $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_HEADER, false); 
+    curl_setopt($ch, CURLOPT_POST, count($postData));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+ 
+    $output=curl_exec($ch);
+ 
+    curl_close($ch);
+    return $output;
+ 
+}
+
+$res = send('http://dev.services.flatyou.it/test', ['testA'=>'1','testB'=>'2']);
+echo $res;
+
+?>
--- services/src/routes.php
+++ services/src/routes.php
@@ -5,10 +5,16 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function (Request $request, Response $response, array $args) 
+{
+    $hostname = 'http://' . $request->getUri()->getHost();
+    echo '<h1>Flatyou Services</h1>';
+    echo '<h3><a href="' . $hostname . '">' . $hostname . '</a></h3>';
+});
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->post('/test', function (Request $request, Response $response, array $args) 
+{
+    $data = $request->getParams();
+    return $response->withJson($data);
+    //return $response->getBody()->write('Hello!');
 });
