f4068dc7f2e5cc499a50f8b41ad2a761717d6910
Gastone Penzo <gastone.penzo@gmail.com>
2019-03-21 14:06:55 +0100

    Aggiunto composer twig

--- src/public/index.php
+++ src/public/index.php
@@ -17,6 +17,23 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
 // Load db class
 require __DIR__ . '/../src/db.php';
 
